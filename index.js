4dcce716721d65a3d7e74329eea3caf1f85ee25c
Harrison Powers <harrisonpowers@gmail.com>
2017-01-11 11:51:17 -0500

    First cut

---
+++ index.js
@@ -0,0 +1,15 @@
+#!/usr/bin/env node
+var xlsx = require('xlsx');
+require('exit-on-epipe');
+
+process.stdin.pipe(require('concat-stream')(data => {
+  var d;
+  try {
+    d = xlsx.read(data, {/* cellNF: true, cellFormula: true, cellNF: true, cellStyles: true */});
+  } catch(e) {
+    console.log(e);
+    outbound.emit('error', new Error('Error importing Excel file'));
+  }
+  var csv = xlsx.utils.make_csv(d.Sheets[d.SheetNames[0]], { FS:",", RS:"\n" });
+  console.log(csv);
+}));
